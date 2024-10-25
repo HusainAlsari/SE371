3a48db659c9bb47c2c9685bc314103f547f6faf0
HusainAlsari <165272884+HusainAlsari@users.noreply.github.com>
2024-10-25 07:55:17 +0300

    Delete personal project directory

--- personal project/01Tut/math.js
+++
@@ -1,8 +0,0 @@
-const add =(a,b)=>a+b
-const sub =(a,b)=>a-b
-const mul =(a,b)=>a*b
-const div =(a,b)=>a/b
-//crated functions to export them in the server using the line below
-module.exports =(add,sub,div,mul)
-
-console.log(add(1,2))
