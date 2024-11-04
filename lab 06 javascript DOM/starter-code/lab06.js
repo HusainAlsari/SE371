0c41363de6185d8cf9e8b4f40fa5bf9489e182f7
HusainAlsari <hussainsori88@gmail.com>
2024-11-04 07:27:48 +0300

    commit

--- lab 06 javascript DOM/starter-code/lab06.js
+++ lab 06 javascript DOM/starter-code/lab06.js
@@ -1,6 +1,12 @@
 const run = () => {
-  alert("button pressed");
-};
+  const city = document.getElementById("cities_selector").selectedOptions[0].value;
+  employees_displayed = [];
+  for (let e of data) {
+      if ((e.address.city === city) || (city === "all")) {
+          employees_displayed.push(e);
+      }
+  }
+  createTable(employees_displayed);};
 
 
 for (let d of data) {
@@ -27,13 +33,7 @@ for(let emp of data){
   employeeDisplayed.push(emp);
 }
 
-const createTable=(employeeDisplayed)=>{
-  const tb = document.createElement("table");
-  tb.innerHTML = "";
-  for(let emp of employeeDisplayed){
-    tb.appendChild(createRow(emp));
-  }
-}
+
 document.addEventListener("DOMContentLoaded", ()=>{
 createTable(employeeDisplayed);
 const citiesSelector =document.getElementById("citiesSelector");
@@ -43,4 +43,38 @@ opt_all.value = "all";
 opt_all.appendChild(document.createTextNode("all"));
 citiesSelector.appendChild(opt_all);
 });
+const applyCSSClassNamesToElementsBySelector = (selectorString, ...classes) => {
+  const elems = document.querySelectorAll(selectorString);
+  for (let e of elems) {
+      for (let c of classes) {
+          e.classList.add(c);
+      }
+  }
+};
+
+const createTable = (listOfEmployeesToBeDisplayed) => { 
+  const table_body = document.getElementById("employee_table_body");
+  table_body.innerHTML = "";
+
+  for (let emp of listOfEmployeesToBeDisplayed) {
+      table_body.appendChild(createRow(emp)); 
+  }
+
+  applyCSSClassNamesToElementsBySelector("tr:nth-child(even)", "grey");
+  applyCSSClassNamesToElementsBySelector("table, td", "border");
+};
+var cities_set = new Set();
+for (let emp of data) {
+    cities_set.add(emp.address.city);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    for (let c of cities_set) {
+        let opt = document.createElement("option");
+        opt.value = c;
+        opt.appendChild(document.createTextNode(c));
+        cities_selector.appendChild(opt);
+    }
+});
+
 
