84701c9a1c1115f385b87514b8809aea172269a9
HusainAlsari <hussainsori88@gmail.com>
2024-11-04 09:38:45 +0300

    commit

---
+++ Class work/loginform/loginLogic.js
@@ -0,0 +1,19 @@
+function check() {
+    let uname = document.getElementById("uname").value;
+    let pword = document.getElementById("pword").value;
+
+    if (!uname && !pword) {
+        alert("Please enter both username and password.");
+    } 
+    else if(!uname){
+        alert("Please enter username.");
+
+    }
+    else if(!pword){
+        alert("Please enter password.");
+
+    }
+    else {
+        alert("Welcome");
+    }
+}
