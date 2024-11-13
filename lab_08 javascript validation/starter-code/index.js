885df331765ea286d39f8377a2cd6ec922c3bc82
HusainAlsari <hussainsori88@gmail.com>
2024-11-13 13:07:21 +0300

    commit

--- lab_08 javascript validation/starter-code/index.js
+++ lab_08 javascript validation/starter-code/index.js
@@ -22,3 +22,27 @@ validateInput(evt, "email",
     "The email has to be a valid PSU email!", 
     /^[a-zA-Z0-9_\-\.]+@psu\.edu\.sa$/
 );
+
+validateInput(evt,
+    "firstName",
+    "firstName_err",
+    "The First name is required!!!!!!",
+    "The first name has to have from 2 to 20 chracters",
+    /^[a-zA-Z]{2,20}$/
+);
+
+validateInput(evt,
+    "lastName",
+    "lastName_err",
+    "the last name is required",
+    "The last name has to have from 2 to 20 chracters",
+    /^[a-zA-Z]{2,20}$/
+)
+
+validateInput(evt,
+    "phoneNumber",
+    "phoneNumber_err",
+    "The phone number is required!!!",
+    "The phone number has to have from 10 to 14 didgits",
+    /^[0-9]{10,14}$/
+)
