da249e8d1a3887de2c009ba1af5c4c464ce26a65
HusainAlsari <hussainsori88@gmail.com>
2024-11-25 09:35:33 +0300

    commit

--- lab_09_express_ejs/starting_code/app.js
+++ lab_09_express_ejs/starting_code/app.js
@@ -7,17 +7,27 @@ require('dotenv').config();
 const app = express();
 
 // Add the public folder as a static files folder (so that files are accessible to client requests)
-app.use(); 
+app.use(express.static('public')); 
 
 // register view engine to express app
-...
+app.set('view engine','ejs');
 
 // Add routes handlers below
-app.METHOD() ...
+
+  app.get('/', (req, res) => {
+    response.render('index', {data1: value1, data2: value2});
+  })
+
+  app.get('/about', (req, res) => {
+    response.render('about', {data1: value1, data2: value2});
+  })
+
 
 
 // Finally if no route corresponds to request render 404 page
-app.use......
+app.use((req,res)=>{
+  res.status(404).render('404')
+})
 
 
 // listen for requests
