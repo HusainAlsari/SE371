469b055b92dd65136eef375bf6bebf3b1428f76c
HusainAlsari <hussainsori88@gmail.com>
2024-12-02 22:34:01 +0300

    commit

--- project/project-part 2/app.js
+++ project/project-part 2/app.js
@@ -14,14 +14,30 @@ app.set('view engine','ejs');
 
 // Add routes handlers below
 
-  app.get('/', (req, res) => {
-    response.render('index', {data1: value1, data2: value2});
-  })
+// Corrected routes handlers
+app.get('/', (req, res) => {
+  const value1 = "Welcome to the Homepage";
+  const value2 = "Enjoy your stay!";
+  res.render('main', { data1: value1, data2: value2 });
+});
 
-  app.get('/about', (req, res) => {
-    response.render('about', {data1: value1, data2: value2});
-  })
+app.get('/about', (req, res) => {
+  const value1 = "About Us";
+  const value2 = "We are committed to excellence.";
+  res.render('about', { data1: value1, data2: value2 });
+});
 
+app.get('/register', (req, res) => {
+  const value1 = "Register Here";
+  const value2 = "Join our community.";
+  res.render('register', { data1: value1, data2: value2 });
+});
+
+app.get('/helpPage', (req, res) => {
+  const value1 = "Help Page";
+  const value2 = "We're here to assist you.";
+  res.render('helpPage', { data1: value1, data2: value2 });
+});
 
 
 // Finally if no route corresponds to request render 404 page
---
+++ project/project-part 2/javaScript/model.js
@@ -0,0 +1,23 @@
+const db = require("../config/database");
+const { DataTypes } = require('sequelize');
+
+// Define the Country model
+const Country = db.sequelize.define('Country', {
+    id:{
+        type: DataTypes.INTEGER,
+        primaryKey: true
+    },
+    name:{
+        type: DataTypes.STRING,
+        unique:true
+    },
+    phone_cdoe:{
+        type: DataTypes.STRING,
+        unique:true
+    }
+
+});
+
+db.sequelize.sync({ alter: true });
+
+module.exports = { Country };
