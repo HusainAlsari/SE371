f981f5540a92c14188edab90363bca51c7ac9326
HusainAlsari <hussainsori88@gmail.com>
2024-12-03 20:46:14 +0300

    commit

--- project/project-part 2/javaScript/model.js
+++ project/project-part 2/javaScript/model.js
@@ -2,22 +2,59 @@ const db = require("../config/database");
 const { DataTypes } = require('sequelize');
 
 // Define the Country model
-const Country = db.sequelize.define('Country', {
-    id:{
+const PatientRegistrationTable = db.sequelize.define('PatientRegistrationTable', {
+    Pid:{
         type: DataTypes.INTEGER,
+        autoIncrement: true, // Auto-generate the Pid
         primaryKey: true
     },
-    name:{
+    first_name:{
         type: DataTypes.STRING,
         unique:true
     },
-    phone_cdoe:{
+    first_last:{
         type: DataTypes.STRING,
+        unique:true
+    },
+    email:{
+        type: DataTypes.STRING,
+        unique:true
+    },
+    phone_number:{
+        type: DataTypes.STRING,
+        unique:true
+    },
+    deprtment:{
+        type:DataTypes.STRING,
         unique:true
     }
 
 });
+//Technical issue form Table
+const Technical_issue = db.sequelize.define('Technical_issue', {
+    issue_id:{
+        type: DataTypes.INTEGER,
+        autoIncrement: true, // Auto-generate the Pid
+        primaryKey: true
+    },
+    first_name:{
+        type: DataTypes.STRING,
+        unique:true
+    },
+    first_last:{
+        type: DataTypes.STRING,
+        unique:true
+    },
+    phone_number:{
+        type: DataTypes.STRING,
+        unique:true
+    },
+    deprtment:{
+        type:DataTypes.STRING,
+        unique:true
+    }
+});
 
 db.sequelize.sync({ alter: true });
 
-module.exports = { Country };
+module.exports = {PatientRegistrationTable, Technical_issue };
